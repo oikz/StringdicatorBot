f8d7485a0f183408f8631e27e33b4e880fb79843
oikz <chapmanjgm@gmail.com>
2021-04-08 18:56:13 +1200

    Starting Point - Token env variable - Discord.NET - Googleapi for search

---
+++ DotEnv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Stringdicator {
+    //Yoinked from https://dusted.codes/dotenv-in-dotnet
+    public static class DotEnv {
+        public static void Load(string filePath) {
+            if (!File.Exists(filePath)) {
+                return;
+            }
+
+            foreach (string line in File.ReadAllLines(filePath)) {
+                string[] parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2) {
+                    continue;
+                }
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+}
---
+++ Stringdicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Stringdicator {
+    class Stringdicator {
+        public static void Main(string[] args)
+            => new Stringdicator().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync() {
+            //Load Token from env file
+            string root = Directory.GetCurrentDirectory();
+            string dotenv = Path.Combine(root, ".env");
+            DotEnv.Load(dotenv);
+            var toge = Environment.GetEnvironmentVariable("TOKEN");
+            int hoge = 0;
+
+        }
+
+        private Task Log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
