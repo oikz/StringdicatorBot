f45c85de5ab77d9f19b94be6a6ea923342177417
oikz <chapmanjgm@gmail.com>
2021-04-08 23:05:08 +1200

    Stringsearch basic stuffs Limited to 10 results in search

--- Modules.cs
+++ Modules.cs
@@ -5,55 +5,89 @@ using Discord;
 using Discord.Commands;
 using GoogleApi;
 using GoogleApi.Entities.Search;
+using GoogleApi.Entities.Search.Common;
 using GoogleApi.Entities.Search.Image.Request;
 
 namespace Stringdicator {
-    
     //Take the users message, delete it and then replace it with Stringdicators own message of the same content
     public class SayModule : ModuleBase<SocketCommandContext> {
         // !say hello world -> hello world
         [Command("say")]
-        [Summary("Delets and then echoes a message.")]
+        [Summary("Deletes and then echoes a message.")]
         public async Task SayAsync([Remainder] [Summary("The text to echo")]
             string echo) {
-            
             //Delete the previous message and then send the requested message afterwards
             var messages = Context.Channel.GetMessagesAsync(1).Flatten();
             await foreach (var hoge in messages) {
                 await Context.Channel.DeleteMessageAsync(hoge, RequestOptions.Default);
             }
-            
+
             await Context.Channel.SendMessageAsync(echo);
         }
     }
 
     public class StringModule : ModuleBase<SocketCommandContext> {
         [Command("string")]
-        [Summary("Finds a string message")]
+        [Summary("Finds a string image")]
         public async Task StringAsync() {
             //Setup and send search request
             ImageSearchRequest request = new ImageSearchRequest();
             request.Query = "Ball of String";
+            //request.Options.Number = 100; //TODO must be below 10
             request.Key = Environment.GetEnvironmentVariable("API_KEY");
             request.SearchEngineId = Environment.GetEnvironmentVariable("SEARCH_ENGINE_ID");
-            
+
             //Gets the search response - contains info about search
             BaseSearchResponse response =
                 await GoogleSearch.ImageSearch.QueryAsync(request);
+
             
             //Pick a random search result
             var items = response.Items.ToArray();
             Random random = new Random();
             var item = items[random.Next(0, items.Length)];
-            
+
             //Create an embed using that image url
             EmbedBuilder builder = new EmbedBuilder();
             builder.WithTitle("String!");
             builder.WithImageUrl(item.Link);
+            builder.WithColor(3447003);
 
-            //Send messahe
+            //Send message
             await Context.Channel.SendMessageAsync("", false, builder.Build());
+            Console.WriteLine("String! - " + item.Link);
         }
     }
 
+    public class StringSearchModule : ModuleBase<SocketCommandContext> {
+        [Command("stringsearch")]
+        [Summary("Finds a searched image")]
+        public async Task StringAsync([Remainder] string searchterm) {
+            //Setup and send search request
+            ImageSearchRequest request = new ImageSearchRequest();
+            request.Query = searchterm;
+            //request.Options.Number = 100; TODO must be below 10
+            request.Key = Environment.GetEnvironmentVariable("API_KEY");
+            request.SearchEngineId = Environment.GetEnvironmentVariable("SEARCH_ENGINE_ID");
+
+            //Gets the search response - contains info about search
+            BaseSearchResponse response =
+                await GoogleSearch.ImageSearch.QueryAsync(request);
+
+            //Pick a random search result
+            var items = response.Items.ToArray();
+            Random random = new Random();
+            var item = items[random.Next(0, items.Length)];
+
+            //Create an embed using that image url
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Stringsearch!");
+            builder.WithImageUrl(item.Link);
+            builder.WithColor(3447003);
+
+            //Send message
+            await Context.Channel.SendMessageAsync("", false, builder.Build());
+            Console.WriteLine("Stringsearch! - " + searchterm + " " + item.Link);
+        }
+    }
 }
