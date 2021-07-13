507636bf37d0f4df21fd23b90ff5aa35f9658a4e
oikz <chapmanjgm@gmail.com>
2021-07-13 14:59:07 +1200

    Remove unused NuGet Packages

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
