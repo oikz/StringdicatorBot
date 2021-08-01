bc7ec4e99450fbe29e5eea9e3d7807a640fd76fb
oikz <chapmanjgm@gmail.com>
2021-08-01 14:49:54 +1200

    Changed prediction server again

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -189,7 +189,7 @@ namespace Stringdicator {
 
             //Prediction endpoint
             const string url =
-                "https://string2.cognitiveservices.azure.com/customvision/v3.0/Prediction/f598b65b-19f1-48fa-a15b-097704cc5e76/classify/iterations/String/image";
+                "https://string3.cognitiveservices.azure.com/customvision/v3.0/Prediction/b0ad2694-b2da-4342-835e-26d7bf6018fc/classify/iterations/String/image";
 
             // Sends the image as a byte array to the endpoint to run a prediction on it
             var byteData = GetImageAsByteArray(imageFilePath);
