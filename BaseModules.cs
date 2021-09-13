e6fc9c788ac029e480146ace34e763a0d5e73979
oikz <chapmanjgm@gmail.com>
2021-09-13 23:19:44 +1200

    Renamed to BaseModules.cs Cleaned up code and added extra comments

--- BaseModules.cs
+++ BaseModules.cs
@@ -1,38 +1,36 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
-using Discord.Audio;
 using Discord.Commands;
 using GoogleApi;
 using GoogleApi.Entities.Search;
 using GoogleApi.Entities.Search.Image.Request;
 
 namespace Stringdicator {
+    /**
+     * Basic help module that outputs available commands to users
+     */
     public class HelpModule : ModuleBase<SocketCommandContext> {
         [Command("Stringdicator")]
         [Summary("Outputs all commands")]
         public async Task HelpAsync() {
             //Kinda jank but gets all the commands again
-            CommandService _commands = new CommandService();
-            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+            var commands = new CommandService();
+            await commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
                 services: null);
 
             //Embed builder
-            EmbedBuilder builder = new EmbedBuilder();
+            var builder = new EmbedBuilder();
             builder.WithTitle("All Stringdicator Commands");
             builder.WithThumbnailUrl("attachment://string.jpg");
             builder.WithColor(3447003);
             builder.WithDescription("");
 
-            foreach (CommandInfo command in _commands.Commands) {
+            foreach (var command in commands.Commands) {
                 //builder.Description += command.Name + ": " + command.Summary + "\n";
-                var fieldBuilder = new EmbedFieldBuilder();
-                fieldBuilder.Name = command.Name;
-                fieldBuilder.Value = command.Summary;
+                var fieldBuilder = new EmbedFieldBuilder {Name = command.Name, Value = command.Summary};
                 builder.AddField(fieldBuilder);
             }
 
@@ -41,14 +39,16 @@ namespace Stringdicator {
     }
 
 
-
+    /**
+     * String! - Googles "ball of string" and returns one of the top 190 images
+     */
     public class StringModule : ModuleBase<SocketCommandContext> {
         [Command("String")]
         [Summary("Finds a string image")]
         public async Task StringAsync() {
             //Setup and send search request
-            ImageSearchRequest request = new ImageSearchRequest();
-            Random random = new Random(); //Nice
+            var request = new ImageSearchRequest();
+            var random = new Random(); //Nice
             request.Options.StartIndex =
                 random.Next(0, 190); //Max limit may change and result in 400 Bad Request responses
             request.Query = "Ball of String";
@@ -71,7 +71,7 @@ namespace Stringdicator {
             var item = items[index];
 
             //Create an embed using that image url
-            EmbedBuilder builder = new EmbedBuilder();
+            var builder = new EmbedBuilder();
             builder.WithTitle("String!");
             builder.WithImageUrl(item.Link);
             builder.WithColor(3447003);
@@ -82,15 +82,19 @@ namespace Stringdicator {
         }
     }
 
+    /**
+     * StringSearch - Googles the search term and returns a random result from the top 10 images
+     */
     public class StringSearchModule : ModuleBase<SocketCommandContext> {
         [Command("Stringsearch")]
         [Summary("Finds a searched image")]
-        public async Task StringAsync([Remainder] string searchterm) {
+        public async Task StringAsync([Remainder] string searchTerm) {
             //Setup and send search request
-            var request = new ImageSearchRequest();
-            request.Query = searchterm;
-            request.Key = Environment.GetEnvironmentVariable("API_KEY");
-            request.SearchEngineId = Environment.GetEnvironmentVariable("SEARCH_ENGINE_ID");
+            var request = new ImageSearchRequest {
+                Query = searchTerm,
+                Key = Environment.GetEnvironmentVariable("API_KEY"),
+                SearchEngineId = Environment.GetEnvironmentVariable("SEARCH_ENGINE_ID")
+            };
 
             //Gets the search response - contains info about search
             var response =
@@ -109,62 +113,12 @@ namespace Stringdicator {
 
             //Send message
             await Context.Channel.SendMessageAsync("", false, builder.Build());
-            Console.WriteLine("Stringsearch! - " + searchterm + " " + item.Link);
-            
+            Console.WriteLine("Stringsearch! - " + searchTerm + " " + item.Link);
+
             //Do an image classification prediction for stringsearch images as well
             CommandHandler.MakePrediction(item.Link, Context);
             GC.Collect();
             //Call the prediction method
-        }
-    }
-
-
-    /**
-     * Base class that holds the audio stuffs
-     */
-    public class AudioAssistModule : ModuleBase<SocketCommandContext> {
-        private Process CreateStream(string path) {
-            return Process.Start(new ProcessStartInfo {
-                FileName = "ffmpeg",
-                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-            });
-        }
-
-        /**
-         * Joins the users channel and plays the specified url, then disconnects
-         */
-        public async Task VoiceAsync(string url) {
-            if (Context.User.IsBot) {
-                return;
-            }
-
-            var channel = (Context.User as IGuildUser).VoiceChannel;
-            if (channel == null) {
-                await ReplyAsync("User not in a voice channel");
-                return;
-            }
-
-            var audioClient = await channel.ConnectAsync();
-
-            var ffmpeg = CreateStream(url);
-            var output = ffmpeg.StandardOutput.BaseStream;
-            var discord = audioClient.CreatePCMStream(AudioApplication.Mixed);
-            try {
-                await output.CopyToAsync(discord);
-            } finally {
-                await discord.FlushAsync();
-                await channel.DisconnectAsync();
-            }
-        }
-    }
-
-    public class TestAudioModule : AudioAssistModule {
-        [Command("test", RunMode = RunMode.Async)]
-        [Summary("Plays a specified audio file")]
-        public async Task PlayAudio() {
-            await VoiceAsync("audio url");
         }
     }
 }
