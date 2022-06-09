b5cb6b85fa9de992bbe110493aa7788ba51259b4
oikz <chapmanjgm@gmail.com>
2022-06-09 18:56:11 +1200

    Document the User and Channel Models

--- Database/Channel.cs
+++ Database/Channel.cs
@@ -1,7 +1,21 @@
 ﻿namespace Stringdicator.Database;
 
+/// <summary>
+/// Channel model representing a single Discord channel in a server.
+/// </summary>
 public class Channel {
+    /// <summary>
+    /// The server's Discord Id, also the Primary Key.
+    /// </summary>
     public ulong Id { get; set; }
+    
+    /// <summary>
+    /// Whether or not this server has been blacklisted from commands.
+    /// </summary>
     public bool Blacklisted { get; set; }
+    
+    /// <summary>
+    /// Whether or not this server has been blacklisted from Image Classification checks.
+    /// </summary>
     public bool ImageBlacklisted { get; set; }
 }
--- Database/User.cs
+++ Database/User.cs
@@ -1,8 +1,22 @@
 ﻿namespace Stringdicator.Database; 
 
+/// <summary>
+/// The User model represents a Discord user, with their ID and any additional data fields related to that user.
+/// E.g., The number of No Anime Violations and the number of Gorilla Moments they have.
+/// </summary>
 public class User {
+    /// <summary>
+    /// The user's Discord Id, also the Primary Key.
+    /// </summary>
     public ulong Id { get; set; }
-
+    
+    /// <summary>
+    /// The number of No Anime Violations that the user has received.
+    /// </summary>
     public int Violations { get; set; }
+    
+    /// <summary>
+    /// The number of Gorilla Moments that the user has received.
+    /// </summary>
     public int GorillaMoments { get; set; }
 }
